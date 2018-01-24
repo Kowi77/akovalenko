c0224e68b008d4740b4d678e5a2bee559f4cc6a6
Kowi77 <kovalenka1977@gmail.com>
2018-01-24 10:25:02 +0700

    Map without override

---
+++ chapter_001/src/test/java/ru/job4j/map/MapTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+
+    Map<User, Object> users;
+    User user1;
+    User user2;
+
+    @Before
+    public void setUp() {
+        users = new HashMap();
+        user1 = new User("First", 1, new GregorianCalendar(2000, 12, 20));
+        user2 = new User("First", 1, new GregorianCalendar(2000, 12, 20));
+        users.put(user1, new Object());
+        users.put(user2, new Object());
+    }
+
+
+    @Test
+    public void checkMapWithoutOverrideEqualsAndHashcode() {
+        System.out.println(users);
+        Assert.assertEquals(users.size(), 2);
+    }
+}
